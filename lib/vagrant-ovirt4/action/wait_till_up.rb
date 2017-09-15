13d39ed04f6b78a29324c1d7520016b70bb1be2f
Christian Grundmann <christian.grundmann@fabasoft.com>
2017-09-15 09:47:20 +0200

    added check for ssh port open

--- lib/vagrant-ovirt4/action/wait_till_up.rb
+++ lib/vagrant-ovirt4/action/wait_till_up.rb
@@ -1,6 +1,8 @@
 require 'log4r'
 require 'vagrant-ovirt4/util/timer'
 require 'vagrant/util/retryable'
+require 'socket'
+require 'timeout'
 
 module VagrantPlugins
   module OVirtProvider
@@ -14,6 +16,23 @@ module VagrantPlugins
         def initialize(app, env)
           @logger = Log4r::Logger.new("vagrant_ovirt4::action::wait_till_up")
           @app = app
+        end
+
+        def port_open?(ip, port, seconds=10)
+          # => checks if a port is open or not on a remote host
+          Timeout::timeout(seconds) do
+            begin
+              TCPSocket.new(ip, port).close
+              @logger.info("SSH Check OK for IP: #{ip}")
+              true
+            rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError => e
+              @logger.info("SSH Connection Failed for IP #{ip}: #{e}")
+              false
+            end
+          end
+          rescue Timeout::Error
+            @logger.info("SSH Connection Failed: Timeout for IP: #{ip}" )
+            false
         end
 
         def call(env)
@@ -40,9 +59,13 @@ module VagrantPlugins
               nics = nics_service.list
               ip_addr = nics.collect { |nic_attachment| env[:connection].follow_link(nic_attachment).reported_devices.collect { |dev| dev.ips.collect { |ip| ip.address if ip.version == 'v4' } unless dev.ips.nil? } }.flatten.reject { |ip| ip.nil? }.first rescue nil
               unless ip_addr.nil?
-                env[:ip_address] = ip_addr
-                break
-                @logger.debug("Got output #{env[:ip_address]}")
+                env[:ui].info("Got IP: #{ip_addr}")
+                # Check if SSH-Server is up
+                if port_open?(ip_addr, 22)
+                  env[:ip_address] = ip_addr
+                  break
+                  @logger.debug("Got output #{env[:ip_address]}")
+                end
               end
               sleep 5
             end
