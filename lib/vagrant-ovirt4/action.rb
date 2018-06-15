c270285500506e9f4f7ed9dc59726e104f03b3c5
sll552 <sll552@users.noreply.github.com>
2018-06-15 13:56:27 +0200

    Add box handling to get_state
    This is required if e.g. vagrant is run in a Docker container and boxes are missing between creating and destroying vms. 
    Otherwise/Currently the state will be corrupted when running `vagrant status` because the boxes are not downloaded.

--- lib/vagrant-ovirt4/action.rb
+++ lib/vagrant-ovirt4/action.rb
@@ -78,6 +78,7 @@ module VagrantPlugins
       # state is expected to be put into the `:machine_state_id` key.
       def self.action_read_state
         Vagrant::Action::Builder.new.tap do |b|
+          b.use HandleBox
           b.use ConfigValidate
           b.use ConnectOVirt
           b.use ReadState
